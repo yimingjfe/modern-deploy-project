ca725a27fe9f95a8dcc36271fd77dc48fa9e38b0
wangyiming <wangyiming.fe@bytedance.com>
2024-04-30 16:43:10 +0800

    feat: csr + bff

--- modern.config.ts
+++ modern.config.ts
@@ -1,4 +1,6 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
+import { bffPlugin } from '@modern-js/plugin-bff';
+import { expressPlugin } from '@modern-js/plugin-express';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -9,8 +11,17 @@ export default defineConfig({
     appTools({
       bundler: 'webpack', // Set to 'experimental-rspack' to enable rspack ⚡️🦀
     }),
+    bffPlugin(),
+    expressPlugin(),
   ],
   server: {
-    ssr: true,
+    ssr: {
+      mode: 'stream',
+    },
+    ssrByEntries: {
+      one: false,
+      two: false,
+      four: false,
+    },
   },
 });
