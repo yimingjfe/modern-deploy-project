ee1523abd7849ea24308fa4c466f503138c7fda4
wangyiming <wangyiming.fe@bytedance.com>
2024-05-06 15:49:10 +0800

    feat: streaming

--- src/three/routes/user/profile/page.tsx
+++ src/three/routes/user/profile/page.tsx
@@ -1,11 +1,24 @@
-import { Outlet, useLoaderData } from '@modern-js/runtime/router';
+import { Outlet, useLoaderData, Await } from '@modern-js/runtime/router';
+import { Suspense } from 'react';
 
 export default function Page() {
-  const data = useLoaderData() as string;
+  const data = useLoaderData() as {
+    data: Promise<string>;
+  };
   return (
     <div className="user-profile">
-      {data}
-      <Outlet />
+      <Suspense fallback={<div id="loading">loading user data ...</div>}>
+        <Await resolve={data.data}>
+          {user => {
+            return (
+              <div id="data">
+                name: {user.name}, age: {user.age}
+              </div>
+            );
+          }}
+        </Await>
+        <Outlet />
+      </Suspense>
     </div>
   );
 }
