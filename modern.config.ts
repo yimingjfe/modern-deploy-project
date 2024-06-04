5393559796d636abc51d45f951320dc3fd12f6da
wangyiming <wangyiming.fe@bytedance.com>
2024-06-04 14:11:41 +0800

    chore: remove bff

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,4 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
-import { bffPlugin } from '@modern-js/plugin-bff';
-import { expressPlugin } from '@modern-js/plugin-express';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -11,8 +9,6 @@ export default defineConfig({
     appTools({
       bundler: 'experimental-rspack', // Set to 'experimental-rspack' to enable rspack ⚡️🦀
     }),
-    bffPlugin(),
-    expressPlugin(),
   ],
   bff: {
     prefix: '/bff-api',
--- src/one/routes/page.tsx
+++ src/one/routes/page.tsx
@@ -1,41 +1,8 @@
 import './index.css';
-import { configure } from '@modern-js/runtime/bff';
-import { useEffect, useState } from 'react';
-import hello, { postHello } from '@api/index';
-
-configure({
-  interceptor(request) {
-    return async (url, params) => {
-      const res = await request(url, params);
-      return res.json();
-    };
-  },
-});
+import { useState } from 'react';
 
 const Page = () => {
-  const [message, setMessage] = useState('bff-express');
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await hello();
-      setMessage(res.message);
-    };
-
-    fetchData();
-    postHello({
-      params: {
-        id: '1111',
-      },
-      query: {
-        user: 'modern@email.com',
-      },
-      data: {
-        message: '3333',
-      },
-      headers: {
-        'x-header': '3333',
-      },
-    });
-  });
+  const [message] = useState('bff-express');
   return <div className="hello">{message}</div>;
 };
 
